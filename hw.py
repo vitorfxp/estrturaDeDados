3b13b7a49c83a2303c0a687e9021f619fa5a18ac
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2025-08-26 13:24:54 -0400

    commit 2

---
+++ hw.py
@@ -0,0 +1,15 @@
+import string
+import time
+text = 'Hello world, Im gay!'
+temp = ''
+
+for ch in text:
+    for i in string.printable:
+        if i == ch or i == '':
+            time.sleep(0.02)
+            print(temp + i)
+            temp += ch 
+            break
+        else:
+            time.sleep(0.02)
+            print(temp + i)
